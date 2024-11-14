2096c5cf53c5128d3e7ee7ffa5bc50166d56113d
Joan <jdamiaros@gmail.com>
2024-11-14 11:22:48 +0100

    changes on RemotePlayerController

--- Assets/PlatformBrawler/Scripts/RemotePlayerController.cs
+++ Assets/PlatformBrawler/Scripts/RemotePlayerController.cs
@@ -6,10 +6,10 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 
+
 public class RemotePlayerController : MonoBehaviour
 {
-    
-    public bool Apressed, Wpressed, Spressed, Dpressed, Qpressed, Epressed;
+    public RemoteInputs remoteInputs;  
 
     //Paràmetres de moviment del jugador
     [SerializeField] private float movSpeed = 10f;
@@ -28,10 +28,10 @@ public class RemotePlayerController : MonoBehaviour
 
         //Configuració del socket UDP
         udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-        serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000); 
+        serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
         remoteEndPoint = (EndPoint)serverEndPoint;
 
-        
+        //Iniciar el fil per rebre dades
         receiveThread = new Thread(new ThreadStart(ReceiveData));
         receiveThread.Start();
     }
@@ -40,10 +40,10 @@ public class RemotePlayerController : MonoBehaviour
     {
         if (connected)
         {
-            
+            //Enviar inputs actuals al servidor
             SendInputData();
 
-            
+            //Actualitzar moviment del jugador
             MovePlayer();
         }
     }
@@ -52,21 +52,20 @@ public class RemotePlayerController : MonoBehaviour
     {
         Vector3 movement = Vector3.zero;
 
-        if (Apressed) movement += Vector3.left;
-        if (Dpressed) movement += Vector3.right;
-        if (Wpressed) movement += Vector3.forward;
+        if (remoteInputs.Apressed) movement += Vector3.left;
+        if (remoteInputs.Dpressed) movement += Vector3.right;
+        if (remoteInputs.Wpressed) movement += Vector3.forward;
 
         rb.MovePosition(rb.position + movement * movSpeed * Time.deltaTime);
-
     }
 
     private void SendInputData()
     {
         //Serialitzar inputs a JSON
-        string jsonData = JsonUtility.ToJson(this);
+        string jsonData = JsonUtility.ToJson(remoteInputs);
         byte[] data = Encoding.ASCII.GetBytes(jsonData);
 
-        
+        //Enviar dades al servidor
         udpSocket.SendTo(data, remoteEndPoint);
     }
 
@@ -78,8 +77,8 @@ public class RemotePlayerController : MonoBehaviour
             int recv = udpSocket.ReceiveFrom(buffer, ref remoteEndPoint);
             string jsonData = Encoding.ASCII.GetString(buffer, 0, recv);
 
-            //Deserialitzar dades
-            JsonUtility.FromJsonOverwrite(jsonData, this);
+            //Deserialitzar dades a remoteInputs
+            JsonUtility.FromJsonOverwrite(jsonData, remoteInputs);
         }
     }
 
@@ -88,5 +87,5 @@ public class RemotePlayerController : MonoBehaviour
         receiveThread.Abort();
         udpSocket.Close();
     }
-}
+} 
 
