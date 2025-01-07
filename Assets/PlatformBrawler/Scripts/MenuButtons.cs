f486ce291ac21ce1fe0132c896c11f53feade131
Bankaster <79161096+Bankaster@users.noreply.github.com>
2025-01-07 17:44:45 +0100

    Main Menu Exit Button Works

---
+++ Assets/PlatformBrawler/Scripts/MenuButtons.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void ExitButton()
+    {
+        //Close the Application
+        Application.Quit();
+    }
+}
