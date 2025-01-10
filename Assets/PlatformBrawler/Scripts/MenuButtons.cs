bcefe2fdc675d544b06b856e8da2d7668600a2ef
Bankaster <79161096+Bankaster@users.noreply.github.com>
2025-01-10 18:52:24 +0100

    New Main Menu Icons effect

--- Assets/PlatformBrawler/Scripts/MenuButtons.cs
+++ Assets/PlatformBrawler/Scripts/MenuButtons.cs
@@ -1,19 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
-public class MenuButtons : MonoBehaviour
+public class MenuButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public Sprite oldSprite;
+    public Image targetSprite;
+    public Sprite newSprite;
+
+    private void Start()
+    {
+        if (targetSprite != null)
+        {
+            oldSprite = targetSprite.sprite;
+        }
+    }
+
+    //Change the image when the mouse is on it
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (targetSprite != null && newSprite != null)
+        {
+            targetSprite.sprite = newSprite;
+        }
+    }
+
+    //Restore the Image to his original sprite
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (targetSprite != null && oldSprite != null)
+        {
+            targetSprite.sprite = oldSprite;
+        }
+    }
+
+    //Close the Game
     public void ExitButton()
     {
-        //Close the Application
         Application.Quit();
     }
 
+    //Load the Main Menu Scene
     public void MainMenuButton()
     {
-        //Load the Main Menu Scene
         SceneManager.LoadScene(0);
     }
 }
